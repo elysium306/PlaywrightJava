f8d1a7962c0823a715805087cc88fbb5305569c1
elysium306 <elysium306@hotmail.com>
2023-12-05 08:22:15 -0600

    Deleted not needed package

--- src/test/java/com/playwright/ui/pages/PlaywrightHome.java
+++ src/test/java/com/playwright/ui/pages/PlaywrightHome.java
@@ -12,25 +12,22 @@ public class PlaywrightHome {
 
 	// 1. String locators _
 	private String get_Started = "getStarted_Sjon";
-
-	private String playwright_title = "a b.navbar__title";
-
 	private String docs = "header div div a[href='/docs/intro']:first-child";
 
 	// 2. Need to create constructor
 	public PlaywrightHome(String browserName) {
 		this.page = PlaywrightFactory.initPlaywright(prop);
 	}
-	
+
 	// 3. Page actions/methods
 	public String getHomePageTitle() {
 		return page.title();
 	}
-	
+
 	public String getHomePageURL() {
 		return page.url();
 	}
-	
+
 	public void search(String productName) {
 		page.fill(docs, productName);
 		page.click(get_Started);
