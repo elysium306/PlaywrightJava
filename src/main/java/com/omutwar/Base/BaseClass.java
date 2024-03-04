0ffd1374329daeee9730c8582126466382041497
elysium306 <elysium306@hotmail.com>
2024-03-04 08:55:42 -0600

    Modified a method

--- src/main/java/com/omutwar/Base/BaseClass.java
+++ src/main/java/com/omutwar/Base/BaseClass.java
@@ -6,19 +6,19 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 public class BaseClass {
-	
-	protected Playwright playwright = Playwright.create();
-	protected Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
-	protected Page page;
-	
+
+    protected Playwright playwright = Playwright.create();
+    protected Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    protected Page page;
+
 //	public BaseClass(Page page, String browserType) {
 //		this.page = page;
 //		
 //	}
-	
-	void openWebsite(Page page, String url) {
-		page = browser.newPage();
-		page.navigate(url);
-	}
+
+    void openApplication(Page page, String url) {
+        page = browser.newPage();
+        page.navigate(url);
+    }
 
 }
